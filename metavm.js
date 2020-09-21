ede6dc24e63c5529976a74d48e89a7fe1e90634f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-21 15:21:35 +0300

    First implementation

--- metavm.js
+++ metavm.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('Module stub');
+const vm = require('vm');
+const fs = require('fs').promises;
+
+const SCRIPT_OPTIONS = { timeout: 5000, displayErrors: false };
+
+const createContext = () => {
+  const context = {};
+  return vm.createContext(context);
+};
+
+class MetaScript {
+  constructor(filePath, context = null) {
+    this.path = filePath;
+    this.context = context || createContext();
+    this.script = null;
+    return this.load();
+  }
+
+  async load() {
+    const src = await fs.readFile(this.path, 'utf8');
+    if (!src) return null;
+    const script = new vm.Script(src, { filename: this.path });
+    this.script = script.runInContext(this.context, SCRIPT_OPTIONS);
+    return this;
+  }
+}
+
+module.exports = { MetaScript };
