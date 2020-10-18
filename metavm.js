120599c66564e667d5015fa761b2794ceb9ca7c2
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-18 13:00:42 +0300

    Decompose vm and loader
    PR-URL: https://github.com/metarhia/metavm/pull/7

--- metavm.js
+++ metavm.js
@@ -1,30 +1,6 @@
 'use strict';
 
-const vm = require('vm');
-const fs = require('fs').promises;
+const vm = require('./lib/vm.js');
+const loader = require('./lib/loader.js');
 
-const SCRIPT_OPTIONS = { timeout: 5000, displayErrors: false };
-
-const createContext = () => {
-  const context = {};
-  return vm.createContext(context);
-};
-
-class MetaScript {
-  constructor(filePath, context = null) {
-    this.path = filePath;
-    this.context = context || createContext();
-    this.script = null;
-    return this.load();
-  }
-
-  async load() {
-    const src = await fs.readFile(this.path, 'utf8');
-    if (!src) return null;
-    const script = new vm.Script(src, { filename: this.path });
-    this.script = script.runInContext(this.context, SCRIPT_OPTIONS);
-    return this;
-  }
-}
-
-module.exports = { MetaScript };
+module.exports = { ...vm, ...loader };
