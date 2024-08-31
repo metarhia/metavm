2057fba9312923805ee54fee2c7a5ba9ba6a5b45
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-08-31 17:24:38 +0300

    Add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,23 @@
 
 const init = require('eslint-config-metarhia');
 
-module.exports = [...init];
+module.exports = [
+  ...init,
+  {
+    files: ['examples/**/*.js', 'examples/**/*.cjs'],
+    rules: {
+      strict: 'off',
+    },
+    languageOptions: {
+      globals: {
+        unknownFunction: true,
+      },
+    },
+  },
+  {
+    files: ['dist/**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+    },
+  },
+];
